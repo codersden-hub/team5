8e766144e48f56f8ddea5eb6d174439d9620de53
Victor Nkire <nkirevictor77@gmail.com>
2023-08-24 06:00:44 +0100

    project folder

---
+++ src/components/header/header.jsx
@@ -0,0 +1,1 @@
+//All of the header content goes here!
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './index.html',
+    './src/**/*.{js,jsx}',
+    './src/components/header/**/*.{js,jsx}',
+    './src/components/body/**/*.{js,jsx}',
+    './src/components/footer/**/*.{js,jsx}'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
