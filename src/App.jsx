00ec95689f888b3ec6ace14b534729cb32864050
Hammond ziggy <zorzorisrael6@gmail.com>
2023-09-02 23:56:46 +0100

    taking shape

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import SignUp from "./pages/SignUp";
 import CreateAccount from "./pages/CreateAccount";
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./pages/Home";
-import MainPage from "./components/body/main";
+import MockData from "./components/body/mockDataComponent";
 import { Routes, Route } from "react-router-dom";
 
 const App = () => {
@@ -11,7 +11,7 @@ const App = () => {
     <>
       <Navbar />
       <Home />
-      <MainPage />
+      <MockData />
       {/* <Routes>
           <Route path="/" element={<Home />}/>
           <Route path="/signin" element={<SignUp />} />
--- src/components/body/main.jsx
+++
@@ -1,9 +0,0 @@
-//This is the main component page!
-import React from 'react';
-import MockData from './mockDataComponent';
-
-function MainPage() {
-    <MockData />
-}
-
-export default MainPage;
