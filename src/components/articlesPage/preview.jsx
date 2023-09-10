87e09dce0ea5d45ee56876d1e163654d7e409e84
James Israel <yongeazi143@gmail.com>
2023-09-10 12:56:13 +0100

    Preview Updated

--- src/components/articlesPage/preview.jsx
+++ src/components/articlesPage/preview.jsx
@@ -1,20 +1,24 @@
-import React from 'react';
-
-const ArticlePreview = ({ title, minContent, category, userName, userImage }) => {
-    return (
-      <div className="max-w-xl mx-auto p-6 bg-white rounded-lg shadow-md">
-        <h2 className="text-2xl font-semibold mb-4">{title}</h2>
-        <img src={image} alt="post" />
-        <div className="flex items-center mb-4">
-          <img src={userImage} alt="User" className="w-10 h-10 rounded-full mr-2" />
-          <span className="text-gray-600">{userName}</span>
-        </div>
-        <p className="text-gray-200 mb-1">-{category}</p>
-  
-        <div className="mb-4" dangerouslySetInnerHTML={{ __html: minContent }} />
-
+import React from "react";
+import { useGlobalContext } from "../context";
+const ArticlePreview = ({ title, minContent, category, userName }) => {
+  const { selectedImg } = useGlobalContext();
+  return (
+    <div className="max-w-xl mx-auto p-6 bg-white rounded-lg shadow-md">
+      <h2 className="text-2xl font-semibold mb-4">{title}</h2>
+      <img src={selectedImg} alt="post" />
+      <div className="flex items-center mb-4">
+        <img
+          src={selectedImg}
+          alt="User"
+          className="w-10 h-10 rounded-full mr-2"
+        />
+        <span className="text-gray-600">{userName}</span>
       </div>
-    );
-  };
-  
-  export default ArticlePreview;
+      <p className="text-gray-200 mb-1">-{category}</p>
+
+      <div className="mb-4" dangerouslySetInnerHTML={{ __html: minContent }} />
+    </div>
+  );
+};
+
+export default ArticlePreview;
